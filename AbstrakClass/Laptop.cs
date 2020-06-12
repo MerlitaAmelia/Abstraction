8499d65eceb26c70b2b647c0370f499a25ffafa3
MerlitaAmelia <merlita.amelia@students.amikom.ac.id>
2020-06-12 22:06:49 +0700

    abstrak

---
+++ AbstrakClass/Laptop.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasAbstrak2693.AbstrakClass
+{
+    public abstract class Laptop
+    {
+        public abstract void menyala();
+    }
+}
---
+++ AbstrakClass/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TugasAbstrak2693.AbstrakClass
+namespace TugasAbstrak2693
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Laptop laptop;
+            laptop = new Acer();
+            laptop.menyala();
+            Console.WriteLine();
+            laptop = new Asus();
+            laptop.menyala();
+            Console.ReadKey();
+           
+        }
+    }
+}
